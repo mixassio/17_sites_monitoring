e7200b06079059472822aa8736d0beb7278d2227
mixassio <mixail.bondarev@gmail.com>
2017-04-21 15:16:42 +0300

    Done

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,6 +1,6 @@
 import os
-import json
-import whois
+import re
+from datetime import datetime, timedelta
 import codecs
 import requests
 import chardet
@@ -12,23 +12,40 @@ def load_urls4check(filepath):
         with codecs.open(filepath, 'rb', encoding=char_type) as file_text:
             return file_text.read().split('\r\n')
 
+"""def TEMP_load_file(filepath):
+    with open(filepath, 'rb') as file_text:
+        char_type = chardet.detect(file_text.read())['encoding']
+    with codecs.open(filepath, 'rb', encoding=char_type) as file_text:
+        result = re.findall(r'Expiry Date: (\d\d\d\d\-\d\d\-\d\d)', file_text.read())
+    date_expiry = datetime.strptime( result[0], "%Y-%m-%d").date()
+    if (date_expiry - timedelta(days=30)) > datetime.date(datetime.now()):
+        date_ok = True
+    else:
+        date_ok = False
+    print(date_ok, type(date_ok))
+"""
+
+
 def is_server_respond_with_200(url):
     url_info = requests.get(url)
     return url_info.status_code
 
 def get_domain_expiration_date(domain_name):
-    domain = requests.get('http://htmlweb.ru/analiz/api.php?whois&url=devman.org&json')
-    print(dir(domain))
-    #print(domain['whois'])
-    j = domain.json()
-    print(j['whois'])
-    print(j['limit'])
-    print(j['creation'])
-    print(j['paid'])
+    domain = requests.get('http://htmlweb.ru/analiz/api.php?whois&url=' + domain_name + '&json')
+    print(domain.json()['limit'])
+    result = re.findall(r'Expiry Date: (\d\d\d\d\-\d\d\-\d\d)', domain.json()['whois'])
+    print(result[0])
+    date_expiry = datetime.strptime(result[0], "%Y-%m-%d").date()
+    if (date_expiry - timedelta(days=30)) > datetime.date(datetime.now()):
+        date_ok = 'OK'
+    else:
+        date_ok = 'less month'
+    return date_expiry, date_ok
 
 if __name__ == '__main__':
     list_url = load_urls4check('./url.txt')
-    l = [line.strip() for line in list_url]
-    print(list_url)
-    print(is_server_respond_with_200('https://devman.org/'))
-    get_domain_expiration_date('google.com')
+    for site in list_url:
+        print(site)
+        status_code = is_server_respond_with_200(site)
+        date_expiry, date_ok = get_domain_expiration_date(site)
+        print('Site: {}\n Status server: {}\n Date expiry: {} (End - {})'.format(site, status_code, date_ok, date_expiry))
